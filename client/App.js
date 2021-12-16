f7a9f192cd97943c596d2bd39b9db872ed16196b
thanhdt300999 <thanh.dt300999@gmail.com>
2021-12-16 17:05:33 +0700

    add home, infomation screen

--- client/App.js
+++ client/App.js
@@ -1,28 +1,33 @@
-import  React from 'react';
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import HomeScreen from './components/Home'
+import InitialScreen from './components/InitialScreen'
+import Certificate from './components/Certificate';
+import Advise from './components/Advise';
+const Stack = createNativeStackNavigator();
 
-const Stack = createStackNavigator();
-
-import Map from "./components/Map"
-import Home from "./components/Home";
-
-function MyTab() {
-    return(
-        <Stack.Navigator>
-            <Stack.Screen
-                name = "Home"
-                component = {Home}
-            />
-        </Stack.Navigator>
-    )
+function App() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+    <NavigationContainer>
+      <Stack.Navigator
+        // screenOptions={{
+        //   headerShown: false
+        // }}
+      >
+        <Stack.Screen options={{headerShown: false}} name="Home" component={HomeScreen} />
+        <Stack.Screen options={{title: "Thông tin cá nhân"}} name="Initial" component={InitialScreen} />
+        <Stack.Screen name="Certificate" component={Certificate} />
+        <Stack.Screen name="Advise" component={Advise} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </SafeAreaView>
+  );
 }
 
-export default function App() {
-    return (
-        <NavigationContainer>
-            <MyTab />
-        </NavigationContainer>
-    );
-}
-
+export default App;
